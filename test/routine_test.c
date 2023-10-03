47470b5e28389fff409407c26aa7d3dbf243e77a
FirestormPenguin <edoardogiubellini@gmail.com>
2023-10-03 17:40:50 +0200

    sistemazione thread

--- test/routine_test.c
+++ test/routine_test.c
@@ -18,6 +18,8 @@ typedef struct s_data
 {
 	int philos_nb;
 	int time_to_eat;
+	int time_to_sleep;
+	int fork;
 	t_philos *philos;
 	t_philos *philos_h;
 	pthread_mutex_t lock;
@@ -62,16 +64,25 @@ void	*init_struct(t_data *data)
 
 void  *routine(t_data *data)
 {
-	printf("enter routine %d\n", data->philos->id);
 	while (data->philos->live == 1)
 	{
-		pthread_mutex_lock(&data->lock);
-		data->philos->eat = 1;
-		printf("philo %d is eating\n", data->philos->id);
-		sleep(data->time_to_eat);
-		pthread_mutex_unlock(&data->lock);
-		data->philos->eat = 0;
-		sleep(data->time_to_eat);
+		if (data->fork == 1 && data->philos->sleep == 0)
+		{
+			pthread_mutex_lock(&data->lock);
+			data->philos->eat = 1;
+			data->fork = 0;
+			printf("philo %d is eating\n", data->philos->id);
+			sleep(data->time_to_eat);
+			pthread_mutex_unlock(&data->lock);
+			data->philos->eat = 0;
+			data->philos->sleep = 1;
+			data->fork = 1;
+		}
+		else if (data->philos->sleep == 1)
+		{
+			sleep (data->time_to_sleep);
+			data->philos->sleep = 0;
+		}
 	}
 	return (NULL);
 }
@@ -82,8 +93,9 @@ void scroll_philos(t_data *data)
 	while (data->philos)
 	{
 		//printf("philo id: %d\nsleep: %d\neat: %d\nthink: %d\nlive: %d\n\n", data->philos->id, data->philos->sleep, data->philos->eat, data->philos->think, data->philos->live);
+		printf("philo id: %d\n", data->philos->id);
 		pthread_create(&data->philos->thread, NULL, &routine, data);
-		//pthread_detach(&data->philos->thread);
+		pthread_detach(&data->philos->thread);
 		data->philos = data->philos->next;
 	}
 }
@@ -102,13 +114,15 @@ int main(int argc, char **argv)
 {
 	t_data		data;
 
-	if (argc != 3)
+	if (argc != 4)
 	{
 		printf("Error: arguments number invalid!\n");
 		return (1);
 	}
 	data.philos_nb = atoi(argv[1]);
 	data.time_to_eat = atoi(argv[2]);
+	data.time_to_sleep = atoi(argv[3]);
+	data.fork = 1;
 	init_struct(&data);
 	pthread_mutex_init(&data.lock, NULL);
 	scroll_philos(&data);
